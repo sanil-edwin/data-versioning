c1a1bfeae3cbbc802d24b91d4f670b5895d8169f
Sanil Edwin <sanil1993@gmail.com>
2025-11-09 11:48:40 +0530

    update paths

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,8 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="sanil-hw2-data-versioning"
+export GCP_PROJECT="hw2-data-versioning-prj"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
